7eda56a184e1ff2a6efe5b1ae495b3d8159c2022
hsinpa <hsinpa@gmail.com>
2019-08-30 00:02:20 +0800

    no message

--- source/javascript/three_trial.js
+++ source/javascript/three_trial.js
@@ -2,26 +2,38 @@ import * as THREE from 'three';
 
 global.THREE = THREE
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
+import { CSS3DRenderer } from 'three/examples/jsm/renderers/CSS3DRenderer';
+import { THREEx } from '../library/threex.htmlmixer';
+
 
 export function ExeThreeTrial() {
 
     const scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     var loader = new GLTFLoader();
-    
+    var container = document.getElementById( 'container' );
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setClearColor("#e5e5e5");
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-    
+        
+    console.log(THREEx);
+	var mixerContext= new THREEx.HtmlMixer.Context(renderer, scene, camera)
+
     window.addEventListener('resize', () => {
         renderer.setSize(window.innerWidth, window.innerHeight);
+
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
+
+        //mixerContext.rendererCss.setSize( window.innerWidth, window.innerHeight )
     });
     
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
     
@@ -67,7 +79,7 @@ export function ExeThreeTrial() {
     function animate() {
         requestAnimationFrame( animate );
         renderer.render( scene, camera );
-        
+
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
     }
